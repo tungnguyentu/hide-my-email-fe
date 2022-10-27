e1a3696e0661b241cd1b51b45ee6eab62203abd1
tungnt24 <nguyentutung24@gmail.com>
2022-10-27 16:11:09 +0700

    update logic validate

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /* LOGIN */
 async function login() {
+    if (validate()){
+        return redirectHome()
+    }
     try {
         var data = new URLSearchParams();
         data.append('username', document.getElementById("email").value);
@@ -30,6 +33,9 @@ async function login() {
 
 /* Register */
 async function register(){
+    if (validate()){
+        return redirectHome()
+    }
     try {
         let _data = {
             email: document.getElementById("email").value,
@@ -77,7 +83,10 @@ async function validate(){
         let res = await fetch("http://privaterelay.asia:8080/auth/users/me", requestOptions);
         result = await res.json();
         if (result.status != 200) {
-            return redirectLogin()
+            redirectLogin()
+            return false
+        } else {
+            return true
         }
     } catch (error) {
         console.log(error);
@@ -105,43 +114,44 @@ async function getForwards() {
 }
 
 async function renderForwards() {
-    validate()
-    let users = await getForwards()
-    let active_forward = '';
-    let deactivate_forward = '';
-    let active = 0;
-    let deactivate = 0;
-    users.forEach(user => {
-        let htmlSegment = `<div class="row" style="width: 1160px;padding-right: 100px;padding-left: 100px;">
-                        <div class="col">
-                            <div class="card" id="forward-card" onclick="redirectToDetail('${user.id}', ${user.is_active})">
-                                <div class="card-body" style="height: 80px;border-bottom-left-radius: 8px;border-bottom-right-radius: 8px;border-top-right-radius: 8px;border-top-left-radius: 8px;border-width: 2px;box-shadow: 6px 6px 2px 2px var(--bs-card-cap-bg);">
-                                    <h4 class="card-title" style="font-family: 'San Francisco Display';">${user.label}</h4>
-                                    <h6 class="text-muted card-subtitle mb-2" style="font-family: 'San Francisco Display';">${user.source}</h6>
+    if (validate()) {
+        let users = await getForwards()
+        let active_forward = '';
+        let deactivate_forward = '';
+        let active = 0;
+        let deactivate = 0;
+        users.forEach(user => {
+            let htmlSegment = `<div class="row" style="width: 1160px;padding-right: 100px;padding-left: 100px;">
+                            <div class="col">
+                                <div class="card" id="forward-card" onclick="redirectToDetail('${user.id}', ${user.is_active})">
+                                    <div class="card-body" style="height: 80px;border-bottom-left-radius: 8px;border-bottom-right-radius: 8px;border-top-right-radius: 8px;border-top-left-radius: 8px;border-width: 2px;box-shadow: 6px 6px 2px 2px var(--bs-card-cap-bg);">
+                                        <h4 class="card-title" style="font-family: 'San Francisco Display';">${user.label}</h4>
+                                        <h6 class="text-muted card-subtitle mb-2" style="font-family: 'San Francisco Display';">${user.source}</h6>
+                                    </div>
+                                </div>
                                 </div>
                             </div>
-                            </div>
-                        </div>
-                        <br>`;
-        if (user.is_active) {
-            active_forward += htmlSegment;
-            active += 1;
-        } else {
-            deactivate_forward += htmlSegment;
-            deactivate += 1;
-        }
-        
-    });
-    let activeDeactivate = document.getElementById("active-deactivate")
-    activeDeactivate.innerText = active + " đang hoạt động, " + deactivate + " không hoạt động";
-    let activeQuantity = document.getElementById("active-quantity")
-    activeQuantity.innerText = active + " địa chỉ email đang hoạt động";
-    let deactivateQuantity = document.getElementById("deactivate-quantity")
-    deactivateQuantity.innerText = deactivate + " địa chỉ email không hoạt động";
-    let active_div = document.getElementById("active-item");
-    active_div.innerHTML = active_forward;
-    let deactivate_div = document.getElementById("deactivate-item");
-    deactivate_div.innerHTML = deactivate_forward;
+                            <br>`;
+            if (user.is_active) {
+                active_forward += htmlSegment;
+                active += 1;
+            } else {
+                deactivate_forward += htmlSegment;
+                deactivate += 1;
+            }
+            
+        });
+        let activeDeactivate = document.getElementById("active-deactivate")
+        activeDeactivate.innerText = active + " đang hoạt động, " + deactivate + " không hoạt động";
+        let activeQuantity = document.getElementById("active-quantity")
+        activeQuantity.innerText = active + " địa chỉ email đang hoạt động";
+        let deactivateQuantity = document.getElementById("deactivate-quantity")
+        deactivateQuantity.innerText = deactivate + " địa chỉ email không hoạt động";
+        let active_div = document.getElementById("active-item");
+        active_div.innerHTML = active_forward;
+        let deactivate_div = document.getElementById("deactivate-item");
+        deactivate_div.innerHTML = deactivate_forward;
+    }
 }
 
 function redirectHome(){
@@ -158,42 +168,43 @@ function redirectLogin(){
 
 /* DETAIL */
 async function detailForward(id) {
-    if (sessionStorage.getItem('status') != null){
-        return redirectLogin()
-    }
-    try {
-        var requestOptions = {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + window.localStorage.getItem("token")
-            }
-        };
-        let url = "http://privaterelay.asia:8080/forwards/" + id
-        let res = await fetch(url, requestOptions);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
+    if (validate()) {
+    
+        try {
+            var requestOptions = {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + window.localStorage.getItem("token")
+                }
+            };
+            let url = "http://privaterelay.asia:8080/forwards/" + id
+            let res = await fetch(url, requestOptions);
+            return await res.json();
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
 async function deactivateForward(id) {
-    validate()
-    try {
-        var requestOptions = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + window.localStorage.getItem("token")
-            }
-        };
-        let url = "http://privaterelay.asia:8080/forwards/"+id+"/deactivate"
-        let res = await fetch(url, requestOptions);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
+    if (validate()){
+        try {
+            var requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + window.localStorage.getItem("token")
+                }
+            };
+            let url = "http://privaterelay.asia:8080/forwards/"+id+"/deactivate"
+            let res = await fetch(url, requestOptions);
+            return await res.json();
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
@@ -215,28 +226,28 @@ async function redirectToHome(){
 }
 
 async function renderDetailForward() {
-    validate()
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get('id')
-    let forward = await detailForward(id);
-    const date = new Date(forward.created_at + "Z")
-    let month = date.getMonth() + 1;
-    console.log(forward);
+    if (validate()) {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const id = urlParams.get('id')
+        let forward = await detailForward(id);
+        const date = new Date(forward.created_at + "Z")
+        let month = date.getMonth() + 1;
+        console.log(forward);
 
-    let createAt = document.getElementById("createAt");
-    createAt.innerHTML = "Bạn đã tạo email này vào " +  date.getDate() + " tháng " + month + ", " + date.getFullYear();
-    let title = document.getElementById("title");
-    title.innerText = forward.label;
-    let source = document.getElementById("source");
-    source.innerHTML = forward.source;
-    let forwardTo = document.getElementById("forward-to");
-    forwardTo.value = forward.destination;
-    let label = document.getElementById("label");
-    label.value = forward.label;
-    let note = document.getElementById("note");
-    note.value = forward.note;
-    
+        let createAt = document.getElementById("createAt");
+        createAt.innerHTML = "Bạn đã tạo email này vào " +  date.getDate() + " tháng " + month + ", " + date.getFullYear();
+        let title = document.getElementById("title");
+        title.innerText = forward.label;
+        let source = document.getElementById("source");
+        source.innerHTML = forward.source;
+        let forwardTo = document.getElementById("forward-to");
+        forwardTo.value = forward.destination;
+        let label = document.getElementById("label");
+        label.value = forward.label;
+        let note = document.getElementById("note");
+        note.value = forward.note;
+    }
 }
 
 function copy()
@@ -272,11 +283,12 @@ async function generateEmail(){
 }
 
 async function renderProxy(){
-    validate()
-    let emailAddress = await generateEmail();
-    console.log(emailAddress);
-    let proxy = document.getElementById("proxy")
-    proxy.innerText = emailAddress["email"];
+    if (validate()) {
+        let emailAddress = await generateEmail();
+        console.log(emailAddress);
+        let proxy = document.getElementById("proxy")
+        proxy.innerText = emailAddress["email"];
+    }
 }
 
 
@@ -311,28 +323,28 @@ async function createForward(){
 /* Deactivate */
 
 async function renderForwardDeactivate() {
-    validate()
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get('id')
-    let forward = await detailForward(id);
-    const date = new Date(forward.created_at + "Z")
-    let month = date.getMonth() + 1;
-    console.log(forward);
+    if (validate()) {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const id = urlParams.get('id')
+        let forward = await detailForward(id);
+        const date = new Date(forward.created_at + "Z")
+        let month = date.getMonth() + 1;
+        console.log(forward);
 
-    let createAt = document.getElementById("createAt");
-    createAt.innerHTML = "Bạn đã tạo email này vào " +  date.getDate() + " tháng " + month + ", " + date.getFullYear();
-    let title = document.getElementById("title");
-    title.innerText = forward.label;
-    let source = document.getElementById("source");
-    source.innerHTML = forward.source;
-    let forwardTo = document.getElementById("forward-to");
-    forwardTo.innerHTML = forward.destination;
-    let label = document.getElementById("label");
-    label.value = forward.label;
-    let note = document.getElementById("note");
-    note.value = forward.note;
-    
+        let createAt = document.getElementById("createAt");
+        createAt.innerHTML = "Bạn đã tạo email này vào " +  date.getDate() + " tháng " + month + ", " + date.getFullYear();
+        let title = document.getElementById("title");
+        title.innerText = forward.label;
+        let source = document.getElementById("source");
+        source.innerHTML = forward.source;
+        let forwardTo = document.getElementById("forward-to");
+        forwardTo.innerHTML = forward.destination;
+        let label = document.getElementById("label");
+        label.value = forward.label;
+        let note = document.getElementById("note");
+        note.value = forward.note;
+    }
 }
 
 async function activeForward(){
